affcb45925cda25a0b4479fd346dccb9ff80b3c3
sjproctor <sproctor950@gmail.com>
2020-05-13 09:42:00 -0700

    adds initial buildout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
@@ -10,9 +11,20 @@ class App extends Component{
   }
 
   render(){
+    let square = this.state.squares.map((value, index)=> {
+      return(
+        <Square
+          value={ value }
+        />
+      )
+    })
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <div>
+          { square }
+        </div>
+
       </React.Fragment>
     )
   }
